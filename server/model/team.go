735276a7b18d239018641a6f544131d4eaa84ed5
logica <84759675+logica0419@users.noreply.github.com>
2022-05-16 19:32:28 +0900

    fixed team domain

--- server/model/team.go
+++ server/model/team.go
@@ -1,10 +1,16 @@
 package model
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+)
 
+// チーム
 type Team struct {
-	ID        uuid.UUID
-	ContestID uuid.UUID
-	Users     []User
-	Name      string
+	ID           uuid.UUID    // ID
+	ContestID    uuid.UUID    // コンテストID
+	Users        []User       // 所属ユーザー
+	Name         string       // チーム名
+	IsMerging    bool         // マージ中かどうか(マージ中はできない処理を設けるため)
+	Submissions  []Submission // チームによる全てのサブミッション
+	HighestScore int          // チームの最高スコア
 }
