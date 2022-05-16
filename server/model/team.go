eeb84cc90cb0e7769494050973992794586322a7
logica <84759675+logica0419@users.noreply.github.com>
2022-05-17 03:03:05 +0900

    cut out core info to embed

--- server/model/team.go
+++ server/model/team.go
@@ -4,13 +4,17 @@ import (
 	"github.com/google/uuid"
 )
 
+// チームの基本情報
+type TeamCoreInfo struct {
+	ID   uuid.UUID // ID
+	Name string    // チーム名
+}
+
 // チーム
 type Team struct {
-	ID           uuid.UUID    // ID
-	ContestID    uuid.UUID    // コンテストID
-	Users        []User       // 所属ユーザー
-	Name         string       // チーム名
-	IsMerging    bool         // マージ中かどうか(マージ中はできない処理を設けるため)
-	Submissions  []Submission // チームによる全てのサブミッション
-	HighestScore int          // チームの最高スコア
+	TeamCoreInfo
+	ContestID    uuid.UUID // コンテストID
+	Users        []User    // 所属ユーザー
+	IsMerging    bool      // マージ中かどうか(マージ中はできない処理を設けるため)
+	HighestScore int       // チームの最高スコア
 }
