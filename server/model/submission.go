0dd58d823109070648a123831751ce68ec63f428
logica <84759675+logica0419@users.noreply.github.com>
2022-05-21 01:07:50 +0900

    deleted code url

--- server/model/submission.go
+++ server/model/submission.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"net/url"
 	"time"
 
 	"github.com/google/uuid"
@@ -13,7 +12,6 @@ type Submission struct {
 	Contest     ContestCoreInfo // コンテスト
 	Team        TeamCoreInfo    // チーム
 	Submitter   User            // 提出者
-	CodeURL     url.URL         // コードのURL
 	Comment     string          // コメント
 	Score       float64         // スコア
 	SubmittedAt time.Time       // 提出日時
