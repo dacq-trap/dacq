cab03a791cd1fb7d99ebe51b3fda925c155699f4
oribe(oribe1115) <50443000+oribe1115@users.noreply.github.com>
2022-05-22 20:57:47 +0900

    muiのための設定を追加

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,31 @@
-import '@/styles/globals.css'
+import { CacheProvider } from '@emotion/react'
+import type { EmotionCache } from '@emotion/react'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import createEmotionCache from '@/common/createEmotionCache'
+import '@/styles/globals.css'
+import theme from '@/styles/theme'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSiideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSiideEmotionCache, pageProps } = props
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
 export default MyApp
