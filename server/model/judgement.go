a1c4b568a55d01f4bb697da5dba38be0c5d2f299
logica <84759675+logica0419@users.noreply.github.com>
2022-06-04 03:49:20 +0900

    added id & name to judgement

--- server/model/judgement.go
+++ server/model/judgement.go
@@ -10,6 +10,8 @@ var (
 
 // スコア判定基準
 type Judgement struct {
+	ID             int    // ID
+	Name           string // 判定方法の名前
 	RawScoreScript string // スコア計算スクリプト
 	RawModuleFile  string // 実行時に必要なモジュール管理ファイル
 	PublicSetting  []bool // public / private区分の設定ファイル
