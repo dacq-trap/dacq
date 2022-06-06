4fe4dbb2dd4b24b792cc6c159a547d23cf597813
logica <84759675+logica0419@users.noreply.github.com>
2022-06-06 23:46:28 +0900

    added submitted file ID

--- server/model/submission.go
+++ server/model/submission.go
@@ -11,6 +11,7 @@ type Submission struct {
 	Team        TeamCoreInfo        // チーム
 	Submitter   User                // 提出者
 	Comment     string              // コメント
+	FileID      string              // 提出されたCSVファイルのID
 	Score       float64             // スコア
 	SubmittedAt time.Time           // 提出日時
 }
