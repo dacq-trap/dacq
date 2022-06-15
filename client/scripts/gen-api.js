c0b31271145020cfdf46d362ef8a539da1c815db
oribe(oribe1115) <50443000+oribe1115@users.noreply.github.com>
2022-06-15 18:01:43 +0900

    Apisクラスにまとめるためのスクリプトを追加

--- client/scripts/gen-api.js
+++ client/scripts/gen-api.js
@@ -1,9 +1,11 @@
 const { execSync } = require('child_process')
+const addApis = require('./add-apis')
 
 const SWAGGER_PATH = '../schema/openapi/openapi.yaml'
 const GENERATED_DIR = 'src/api/generated'
 
 ;(async () => {
+  // generate api
   try {
     execSync(
       `npm exec -- @openapitools/openapi-generator-cli generate -g typescript-axios -i ${SWAGGER_PATH} -o ${GENERATED_DIR}`
@@ -11,4 +13,7 @@ const GENERATED_DIR = 'src/api/generated'
   } catch (e) {
     console.error(e)
   }
+
+  // generate Apis class
+  await addApis(GENERATED_DIR)
 })()
