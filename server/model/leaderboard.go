b5b346dc5b89280f4026380cb525a2e5ba1eea38
logica <84759675+logica0419@users.noreply.github.com>
2022-06-17 03:25:45 +0900

    fixed & defined leaderboard

---
+++ server/model/leaderboard.go
@@ -0,0 +1,12 @@
+package model
+
+import "time"
+
+// リーダーボード
+type Leaderboard struct {
+	Rank             int       // 順位
+	Team             Team      // チーム
+	HighestScore     float64   // 最高スコア
+	TotalSubmissions int       // のべ提出数
+	LastSubmittedAd  time.Time // 最終提出日時
+}
