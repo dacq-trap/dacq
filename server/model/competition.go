b9196af1e3a51822a2170cebd8eef966b0ece9b9
logica <84759675+logica0419@users.noreply.github.com>
2022-06-19 04:55:10 +0900

    make some field optional

--- server/model/competition.go
+++ server/model/competition.go
@@ -3,6 +3,7 @@ package model
 import (
 	"time"
 
+	"github.com/dacq-trap/dacq/server/util/optional"
 	"github.com/google/uuid"
 )
 
@@ -17,12 +18,12 @@ type CompetitionCoreInfo struct {
 // コンペティション
 type Competition struct {
 	CompetitionCoreInfo
-	Author          User           // コンペティション作成者
-	Rule            string         // ルール
-	DataID          string         // 学習データファイルID
-	DataDescription string         // 学習データの説明
-	JudgementID     int            // スコア判定基準のID
-	PublicSetting   map[int]bool   // public / private区分の設定
-	AnswerDataID    string         // 正解データファイルID
-	Teams           []TeamCoreInfo // 参加チーム
+	Author          User                // コンペティション作成者
+	Rule            string              // ルール
+	DataID          optional.Of[string] // 学習データファイルID
+	DataDescription string              // 学習データの説明
+	JudgementID     int                 // スコア判定基準のID
+	PublicSetting   map[int]bool        // public / private区分の設定
+	AnswerDataID    string              // 正解データファイルID
+	Teams           []TeamCoreInfo      // 参加チーム
 }
--- server/model/submission.go
+++ server/model/submission.go
@@ -2,16 +2,18 @@ package model
 
 import (
 	"time"
+
+	"github.com/dacq-trap/dacq/server/util/optional"
 )
 
 // サブミッション
 type Submission struct {
-	ID          int                 // ID
-	Competition CompetitionCoreInfo // コンペティション
-	Team        TeamCoreInfo        // チーム
-	Submitter   User                // 提出者
-	Comment     string              // コメント
-	FileID      string              // 提出されたCSVファイルのID
-	Score       float64             // スコア
-	SubmittedAt time.Time           // 提出日時
+	ID          int                  // ID
+	Competition CompetitionCoreInfo  // コンペティション
+	Team        TeamCoreInfo         // チーム
+	Submitter   User                 // 提出者
+	Comment     string               // コメント
+	FileID      string               // 提出されたCSVファイルのID
+	Score       optional.Of[float64] // スコア
+	SubmittedAt time.Time            // 提出日時
 }
