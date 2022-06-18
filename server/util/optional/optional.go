6bc5c4412c910674de82ff49db6ec2099ef4154b
oribe(oribe1115) <50443000+oribe1115@users.noreply.github.com>
2022-06-18 14:57:35 +0900

    ScannerとValuerを満たすための実装

--- server/util/optional/optional.go
+++ server/util/optional/optional.go
@@ -2,7 +2,10 @@ package optional
 
 import (
 	"bytes"
+	"database/sql"
+	"database/sql/driver"
 	"encoding/json"
+	"fmt"
 )
 
 type optionalType interface {
@@ -10,54 +13,93 @@ type optionalType interface {
 }
 
 type Of[T optionalType] struct {
-	Value T
-	Valid bool
+	value T
+	valid bool
 }
 
 func New[T optionalType](value T, valid bool) Of[T] {
 	return Of[T]{
-		Value: value,
-		Valid: valid,
+		value: value,
+		valid: valid,
 	}
 }
 
 func NewWithValue[T optionalType](value T) Of[T] {
 	return Of[T]{
-		Value: value,
-		Valid: true,
+		value: value,
+		valid: true,
 	}
 }
 
 func (o Of[T]) ValueOrZero() T {
-	if o.Valid {
-		return o.Value
+	if o.valid {
+		return o.value
 	}
 
 	return defaultValue[T]()
 }
 
+func (o Of[T]) HasValue() bool {
+	return o.valid
+}
+
 func (o Of[T]) UnmarshalJSON(data []byte) error {
 	if bytes.Equal(data, []byte("null")) {
-		o.Value = defaultValue[T]()
-		o.Valid = false
+		o.value = defaultValue[T]()
+		o.valid = false
 		return nil
 	}
 
-	if err := json.Unmarshal(data, &o.Value); err != nil {
+	if err := json.Unmarshal(data, &o.value); err != nil {
 		return err
 	}
 
-	o.Valid = true
+	o.valid = true
 
 	return nil
 }
 
 func (o Of[T]) MarshalJSON() ([]byte, error) {
-	if o.Valid {
-		return json.Marshal(o.Value)
+	if o.valid {
+		return json.Marshal(o.value)
 	}
 
 	return json.Marshal(nil)
+}
+
+// Scan sql.Scannerを満たすためのメソッド
+func (o Of[T]) Scan(value any) error {
+	switch ((any)(o.value)).(type) {
+	case string:
+		nullString := sql.NullString{}
+		err := nullString.Scan(value)
+		if err != nil {
+			return err
+		}
+		o.value = any(nullString.String).(T)
+		o.valid = nullString.Valid
+	case float64:
+		nullFloat64 := sql.NullFloat64{}
+		err := nullFloat64.Scan(value)
+		if err != nil {
+			return err
+		}
+		o.value = any(nullFloat64.Float64).(T)
+		o.valid = nullFloat64.Valid
+	default:
+		return fmt.Errorf("unsupported type for Of[T].Scan: %T", o.value)
+	}
+
+	return nil
+}
+
+// Value driver.Valuerを満たすためのメソッド
+func (o Of[T]) Value() (driver.Value, error) {
+	if !o.valid {
+		return nil, nil
+	}
+
+	return o.value, nil
 }
 
 func defaultValue[T optionalType]() T {
