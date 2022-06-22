9d224948082951a846408ccf9fe9afc9299f7a01
yumizsui <yumizsui@gmail.com>
2022-06-22 15:48:09 +0900

    markdown-it対応

--- client/src/components/markdown.tsx
+++ client/src/components/markdown.tsx
@@ -1,16 +1,37 @@
 import MarkdownIt from 'markdown-it'
+import katex from '@traptitech/markdown-it-katex'
 import markdownItPrism from 'markdown-it-prism'
+
+import 'prismjs/themes/prism-okaidia.css'
+import 'katex/dist/katex.css'
+
+import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-python'
+import 'prismjs/components/prism-shell-session'
 
 type Props = {
   content: string
 }
 export const MarkDown = (props: Props) => {
-  const testText = '*test* text'
-
-  const md = new MarkdownIt()
+  const md = new MarkdownIt({
+    breaks: true,
+  })
   md.use(markdownItPrism)
+  md.use(katex, { output: 'html' })
 
-  const renderedMd = md.render(testText)
+  md.block.State.prototype.skipEmptyLines = function skipEmptyLines(
+    from: number
+  ): number {
+    for (let max = this.lineMax; from < max; from++) {
+      if (this.bMarks[from]! + this.tShift[from]! < this.eMarks[from]!) {
+        break
+      }
+      this.push('hardbreak', 'br', 0)
+    }
+    return from
+  }
+
+  const renderedMd = md.render(props.content)
 
   return <div dangerouslySetInnerHTML={{ __html: renderedMd }} />
 }
