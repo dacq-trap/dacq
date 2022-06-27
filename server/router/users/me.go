27933f367812dfe84b457427a542ff8efae7fb64
idaten459 <shukai2013@gmail.com>
2022-06-28 00:22:57 +0900

    revise me.go - forbiddenの削除 - elseネストの削除 - errors.Isで判定

--- server/router/users/me.go
+++ server/router/users/me.go
@@ -1,20 +1,24 @@
 package users
 
 import (
-	"github.com/dacq-trap/dacq/server/model"
-	"github.com/labstack/echo/v4"
+	"errors"
 	"net/http"
+
+	"github.com/dacq-trap/dacq/server/model"
+	"github.com/dacq-trap/dacq/server/router/session"
+	"github.com/labstack/echo/v4"
 )
 
 func (h *UsersHandler) GetMe(c echo.Context) error {
 	name := c.Request().Context().Value(session.UserNameKey).(string)
+
 	user, err := h.service.ReadUserByName(name)
-	if err == model.ErrNotFound {
-		return echo.NewHTTPError(http.StatusNotFound)
-	} else if err == model.ErrForbidden {
-		return echo.NewHTTPError(http.StatusForbidden)
-	} else if err != nil {
+	if err != nil {
+		if errors.Is(err, model.ErrNotFound) {
+			return echo.NewHTTPError(http.StatusNotFound)
+		}
 		return echo.NewHTTPError(http.StatusInternalServerError)
 	}
+
 	return c.JSON(http.StatusOK, user)
 }
