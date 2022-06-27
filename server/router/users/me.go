18215d32a9af9807892f7787bd7effa115a078a5
idaten459 <shukai2013@gmail.com>
2022-06-27 23:42:19 +0900

    implement router/users/me.go

--- server/router/users/me.go
+++ server/router/users/me.go
@@ -2,8 +2,35 @@ package users
 
 import (
 	"github.com/labstack/echo/v4"
+	"net/http"
 )
 
+/*
+type: object
+properties:
+  name:
+    type: string
+    description: ユーザー名 (traP ID)
+    example: "trapyojo"
+  iconUrl:
+    type: string
+    description: アイコンURL
+    example: https://q.trap.jp/api/v3/public/icon/trapyojo
+required:
+  - name
+  - iconUrl
+*/
+
+type user struct {
+	Name    string `json:"name"`
+	IconUrl string `json:"iconUrl"`
+}
+
 func (h *UsersHandler) GetMe(c echo.Context) error {
-	return nil
+	name := c.Param("UserName")
+	user := user{
+		Name:    name,
+		IconUrl: "https://q.trap.jp/api/v3/public/icon/" + name,
+	}
+	return c.JSON(http.StatusOK, user)
 }
