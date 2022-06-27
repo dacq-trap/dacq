71ae6e877de246f2cdc374b20fc5e0eae30521f6
TM0428 <55181masui@seiko.ac.jp>
2022-06-27 22:53:31 +0900

    add rank

--- client/src/pages/problems/[id]/leaderboard.tsx
+++ client/src/pages/problems/[id]/leaderboard.tsx
@@ -3,6 +3,7 @@ import ProblemPageBase from '@/components/problemPageBase'
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'rank', headerName: '順位', width: 70 },
   { field: 'firstName', headerName: 'First name', width: 130 },
   { field: 'lastName', headerName: 'Last name', width: 130 },
   {
@@ -23,15 +24,15 @@ const columns: GridColDef[] = [
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, rank: 2, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, rank: 3, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, rank: 1, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, rank: 5, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, rank: 6, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, rank: 4, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, rank: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, rank: 9, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, rank: 8, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
 
@@ -47,10 +48,14 @@ const Leaderboard = (props: Props) => {
       problemPageOption='leaderboard'
     >
       順位表ページ
-      <DataGrid
-        rows={rows}
-        columns={columns}
-      />
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+        />
+      </div>
     </ProblemPageBase>
   )
 }
