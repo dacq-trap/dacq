491c61177713dbbab001d06c607d3b3a091e11f9
idaten459 <shukai2013@gmail.com>
2022-06-30 19:14:37 +0900

    me.competitionIdを実装

---
+++ server/router/teams/me.competitionId.go
@@ -0,0 +1,38 @@
+package teams
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/dacq-trap/dacq/server/model"
+	"github.com/dacq-trap/dacq/server/router/session"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+func (h *TeamsHandler) GetMyTeamInCompetition(c echo.Context) error {
+	competitionId, err := uuid.Parse(c.Param("competitionId"))
+	if err != nil {
+		if errors.Is(err, model.ErrNotFound) {
+			return echo.NewHTTPError(http.StatusNotFound, err)
+		}
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+
+	username := c.Get(session.UserNameKey).(string)
+	team, err := h.service.ReadTeamByUserNameAndCompetitionID(c.Request().Context(), username, competitionId)
+	if err != nil {
+		if errors.Is(err, model.ErrUnauthorized) {
+			return echo.NewHTTPError(http.StatusUnauthorized, err)
+		}
+		if errors.Is(err, model.ErrNotFound) {
+			return echo.NewHTTPError(http.StatusNotFound, err)
+		}
+		return echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+	res := TeamCoreInfo{
+		ID:   team.ID,
+		Name: team.Name,
+	}
+	return c.JSON(http.StatusOK, res)
+}
